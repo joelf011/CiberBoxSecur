a50d68355f76b8101c7dc8e6459f26fd537008ef
Rmorais03 <rodrigo.morais1303@gmail.com>
2026-04-14 12:25:01 +0100

    Initial Setup
    Backend with Node, Express, Postgres and Sequelize
    Frontend with Vite - React + Javascript

---
+++ backend/src/models/database.js
@@ -0,0 +1,6 @@
+var Sequelize = require('sequelize');
+const sequelize = new Sequelize('teste', 'postgres', 'postgres', {host: 'localhost',
+port: '5432',
+dialect: 'postgres',
+});
+module.exports = sequelize;
