09844205e6dbccccb4f2ffa7ed6ef016c99d0103
Rmorais03 <rodrigo.morais1303@gmail.com>
2026-04-21 16:36:59 +0100

    App/Main commit
    Browser routes fix/add and we are making a "Develop" branch from this commit onwards

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* Wrapping the App with the BrowserRouter */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
