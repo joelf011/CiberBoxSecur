2ba6619b89f3fb39fe37b3b7e6799bae911551b3
Joel <joel.f.2003@hotmail.com>
2026-05-19 00:25:53 +0100

    feat(user): implement user profile update and enhance admin user management

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -3,7 +3,15 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
-// Profile route
-router.get('/me', authMiddleware, userController.getProfile);
+router.use(authMiddleware);
+
+router.get('/profile', userController.getProfile);
+router.put('/profile', userController.updateProfile);
+
+// --- ADMIN ---
+// Add here checkPermission('MANAGE_USERS'))
+router.get('/', userController.getAllAdmin);
+router.put('/:id', userController.updateAdmin);
+router.delete('/:id', userController.delete);
 
 module.exports = router;
